a0fbe673441f732f94a8b152eea27238e16ccd4c
Walter Purcaro <vuolter@users.noreply.github.com>
2017-07-11 16:19:28 +0200

    New file structs

--- duplicate/structs.py
+++ duplicate/structs.py
@@ -2,52 +2,91 @@
 
 from __future__ import absolute_import
 
+from collections import namedtuple
+from enum import IntEnum
 from os import lstat
 from os.path import basename
 from stat import S_IFMT, S_IMODE
 
-from .utils import fullpath, is_archived, is_hidden, is_system
+from .utils import fullpath
 
 
-class File(object):
+class GroupFilter(IntEnum):
+    Ident = 0
+    Path = 1
+    Name = 2
+    Mode = 3
+    Type = 4
+    Dev = 5
+    Mtime = 6
+    Size = 7
+    Signature = 8
+    Hash = 9
 
-    __slots__ = ['_archived', '_hidden', '_system',
-                 'name', 'path', 'mode', 'type', 'dev', 'size', 'mtime']
 
-    def __init__(self, name, path=None, st=None):
-        self._archived = None
-        self._hidden = None
-        self._system = None
+class FileGroup(object):
 
-        self.name = basename(name)
-        self.path = path or fullpath(name)
+    __slots__ = ['id', 'type', 'dups', 'errors', 'add_to_errors']
 
-        if not st:
+    def __init__(self, filtertype):
+        self.id = id(self)
+        self.type = filtertype
+        self.dups = {}
+        self.errors = []
+        self.add_to_errors = self.errors.append
+
+    def add_to_dups(self, id, file):
+        dups = self.dups
+        if id in dups:
+            dups[id].append(file)
+        else:
+            dups[id] = [file]
+
+    def _filter(self):
+        dupdict = self.dups
+        for key, value in dupdict.items():
+            if isinstance(value, FileGroup):
+                value._filter()
+                value = value.dups
+            if len(value) < 2:
+                dupdict.pop(key)
+
+    def filter(self, files=None):
+        if files:
+            for file in files:
+                self.add_to_dups(file[self.type], file)
+        self._filter()
+
+    def __str__(self):
+        return """FileGroup(id={0}, type={1})""".format(self.id, self.type)
+
+
+_FileInfo = namedtuple('FileInfo', 'ident path name mode type dev mtime size')
+
+
+class FileInfo(_FileInfo):
+
+    __slots__ = []
+
+    def __new__(cls, name, path=None, st=None):
+        if path is None:
+            path = fullpath(name)
+        if st is None:
             st = lstat(name)
 
-        self.mode = S_IMODE(st.st_mode)
-        self.type = S_IFMT(st.st_mode)
-        self.dev = st.st_dev
-        self.size = st.st_size
+        name = basename(name)
+        mode = S_IMODE(st.st_mode)
+        type = S_IFMT(st.st_mode)
+        dev = st.st_dev
         try:
-            self.mtime = st.st_mtime_ns
+            mtime = st.st_mtime_ns
         except AttributeError:
-            self.mtime = st.st_mtime
+            mtime = st.st_mtime
+        size = st.st_size
+        ident = (type, size)
 
-    def is_archived(self):
-        if self._archived is None:
-            self._archived = is_archived(self.path)
-        return self._archived
-
-    def is_hidden(self):
-        if self._hidden is None:
-            self._hidden = is_hidden(self.path)
-        return self._hidden
-
-    def is_system(self):
-        if self._system is None:
-            self._system = is_system(self.path)
-        return self._system
+        return super(FileInfo, cls).__new__(
+            cls, ident, path, name, mode, type, dev, mtime, size)
 
 
 class SkipException(Exception):
