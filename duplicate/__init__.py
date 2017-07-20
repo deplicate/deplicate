929d459cf591657e81973b47b0eb0b72f558c52f
Walter Purcaro <vuolter@users.noreply.github.com>
2017-07-20 03:58:02 +0200

    Fix __init__

--- duplicate/__init__.py
+++ duplicate/__init__.py
@@ -12,14 +12,14 @@ from __future__ import absolute_import
 
 from .core import cache
 from .deplicate import Deplicate
-from .structs import SkipException
+from .structs import Cache, ResultInfo, SkipException
 from .utils import from_iterable
 
 
 @from_iterable
 def find(*paths, **kwargs):
-    onerror = kwargs.pop('onerror')
-    notify = kwargs.pop('notify')
+    onerror = kwargs.pop('onerror', None)
+    notify = kwargs.pop('notify', None)
 
     d = Deplicate(paths, **kwargs)
     d.find(onerror, notify)
@@ -30,9 +30,9 @@ def find(*paths, **kwargs):
 @from_iterable
 def purge(*paths, **kwargs):
     trash = kwargs.pop('trash', True)
-    onerror = kwargs.pop('onerror')
-    ondel = kwargs.pop('ondel')
-    notify = kwargs.pop('notify')
+    ondel = kwargs.pop('ondel', None)
+    onerror = kwargs.pop('onerror', None)
+    notify = kwargs.pop('notify', None)
 
     d = Deplicate(paths, **kwargs)
     d.purge(trash, ondel, onerror, notify)
